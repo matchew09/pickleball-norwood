ce6575f5ddd9429f413284f9f10a83ae42c20208
Matthew Baradihi <matthewbaradihi@outlook.com>
2023-08-17 16:23:05 -0400

    updates

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const faqItems = document.querySelectorAll(".faq-item");
+
+  faqItems.forEach((item) => {
+    const question = item.querySelector(".faq-question");
+    const answer = item.querySelector(".faq-answer");
+
+    question.addEventListener("click", () => {
+      answer.classList.toggle("show-answer");
+    });
+  });
+});
